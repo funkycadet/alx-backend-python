f46855c8bb23ec87d11df050b1c8cf3a80d1f4e2
funkycadet <ericsmithsonian2@gmail.com>
2023-01-10 14:21:22 +0100

    fix: resolve formatting errors using pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+""" task 0 module """
+import asyncio
 import random
 
-async def wait_random(max_delay: int = 10):
-    i = random.randint(0, max_delay)
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ waits for a random number of seconds """
+    i = random.random() * max_delay
+    await asyncio.sleep(i)
     return i
