eb965fd15f5df767f905d4a70394e388b1be42dc
funkycadet <ericsmithsonian2@gmail.com>
2023-03-20 13:35:10 +0100

    fix: update function documentation

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -7,7 +7,7 @@ import math
 
 def floor(n: float) -> int:
     """
-    function to return a concatenation of two given string arguments
+    function to return the floor of a given float argument
     """
     data = math.floor(n)
     return data
