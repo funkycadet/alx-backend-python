a0c8d7e27ccf5402caff5006d70148a31a497c6d
funkycadet <ericsmithsonian2@gmail.com>
2023-03-20 13:38:02 +0100

    task: add python script with a type-annotated function which returns string repr of floor argument

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to return the string representation of a given float argument
+    """
+    return str(n)
