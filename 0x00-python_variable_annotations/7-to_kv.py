f84c2726bbaa8bf200b9076280b655763a61bb80
funkycadet <ericsmithsonian2@gmail.com>
2023-03-21 00:48:40 +0100

    task: add script with type-annotated function which returns a tuple of string and int/float

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Complex types - string and int/float to tuple
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     function tthat takes a string 'k' and an int OR float 'v' as arguments
     and returns a tuple. The first element of the tuple is the string k. The second
