c170838a7ee9d27afb07aa97881024bb3db2e028
funkycadet <ericsmithsonian2@gmail.com>
2023-03-20 13:52:33 +0100

    task: add python script with predefined annotated variables and arguments

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Define variables
+function to define and annotate given variables with specified values
+"""
+
+a: int = 1
+pi: float = 3.41
+i_understand_annotations: bool = True
+school: str = "Holberton"
