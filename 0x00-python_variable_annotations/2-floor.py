6d85a5704792386055a34d253f2604a50007cb46
funkycadet <ericsmithsonian2@gmail.com>
2023-03-20 13:27:56 +0100

    task: add python script with a type-annotated function which returns floor of given float argument

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    function to return a concatenation of two given string arguments
+    """
+    data = math.floor(n)
+    return data
