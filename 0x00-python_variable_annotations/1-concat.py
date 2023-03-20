7aa3d4334a85263399146abfc0352e12799acf30
funkycadet <ericsmithsonian2@gmail.com>
2023-03-20 13:16:22 +0100

    task: add python script with a type-annotated function which concatenates string arguments

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function to return a concatenation of two given string arguments
+    """
+    return str1 + str2
