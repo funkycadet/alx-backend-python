d348d42c752cdd4ef7182fc87c9cdbb39cb43bfa
funkycadet <ericsmithsonian2@gmail.com>
2023-03-20 13:09:11 +0100

    task: add python script with a type-annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ function that takes two float arguments and returns the sum as float
+    """
+    return a + b
