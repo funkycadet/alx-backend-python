194746ba1f02ef90799fb6d5955d87eaf88da23b
funkycadet <ericsmithsonian2@gmail.com>
2023-03-21 00:47:25 +0100

    task: add script with type-annotated function which returns a tuple of string and int/float

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -11,5 +11,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple:
     and returns a tuple. The first element of the tuple is the string k. The second
     element is the square of the int/float v and would be annotated as a float.
     """
-    v = v ** 2
-    return k, v
+    return (k, float(v ** 2))
